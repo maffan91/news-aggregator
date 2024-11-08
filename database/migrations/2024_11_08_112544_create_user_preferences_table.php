766d0b3ac9bf8ac9a64de8684ebf8b74b4910fa9
Muhammad Affan Khan <affan@Muhammads-MacBook-Air.local>
2024-11-08 16:53:31 +0500

    feat(db): Add user preference table

---
+++ database/migrations/2024_11_08_112544_create_user_preferences_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_preferences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->json('author_ids')->nullable();
+            $table->json('source_ids')->nullable();
+            $table->json('category_ids')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_preferences');
+    }
+};
