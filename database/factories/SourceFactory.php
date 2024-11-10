25ca21ca875ba9613df36393987059d04728f33e
Muhammad Affan Khan <affan@Muhammads-MacBook-Air.local>
2024-11-10 15:17:55 +0500

    fix(test): Randomize source name to avoid unique constraint failure due to faker

--- database/factories/SourceFactory.php
+++ database/factories/SourceFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Source>
@@ -17,7 +18,7 @@ class SourceFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->company() . ' - News API'
+            'name' => fake()->company() . ' - News API' . Str::random(5)
         ];
     }
 }
