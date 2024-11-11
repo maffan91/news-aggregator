2fa141a88caa0467d54a52065f3bc6c9b24e64cb
Muhammad Affan Khan <affan@Muhammads-MacBook-Air.local>
2024-11-11 20:14:17 +0500

    feat(api): Return a simple welcome message on root route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return success message in json
+    return response()->json(['message' => 'Welcome to the News Aggregator API!'], 200);
 });
